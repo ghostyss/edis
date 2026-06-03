9eb2df1e7d3c147cff780c4010e4e13c18c5c535
Ivan1 <gired.bolivia@gmail.com>
2026-06-03 13:10:20 -0400

    upload

--- src/auth-module/locale/i18n.ts
+++ src/auth-module/locale/i18n.ts
@@ -5,12 +5,12 @@ import { initReactI18next } from 'react-i18next';
 i18n
   .use(initReactI18next)
   .init({
-    lng: 'db_idioma', // Nombre clave para tu idioma dinámico
-    fallbackLng: 'db_idioma',
-    resources: {},    // Empieza vacío, se llena en caliente con lo que mande tu PHP
+    lng: 'DBLanguage',
+    fallbackLng: 'DBLanguage',
+    resources: {},
     interpolation: {
       escapeValue: false,
     },
-  } as any); // El 'as any' le quita la rigidez a TypeScript para evitar errores de sobrecarga molestos
+  } as any);
 
 export default i18n;
